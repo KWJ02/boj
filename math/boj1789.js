22d1640abb3b7e2d10f8b2308755e9d217c864d4
dnjs99wo12 <dnjs99wo12@hs.ac.kr>
2025-09-22 10:06:01 +0900

    boj1789 해결

--- math/boj1789.js
+++ math/boj1789.js
@@ -1,22 +1,26 @@
 // 문제 : 수들의 합
-// 94%에서 실패
+// 94%에서 실패 -> dp길이에 0값을 포함한게 문제였음
 const solution = (s) => {
-	const dp = [0, 1];
+	let total = 0;
+	let count = 0;
 
-	if (s <= 2) {
-		console.log(1);
-		return;
+	for (let i = 1; total < s; i++) {
+		total += i;
+		if (total > s) break;
+		count++;
 	}
 
-	for (let i = 2; i + dp[i - 1] <= s; i++) {
-		dp[i] = dp[i - 1] + i;
-	}
-
-	if (dp[dp.length - 1] === s) {
-		console.log(dp.length);
-	} else {
-		console.log(dp.length - 1);
-	}
+	console.log(count);
 };
 
 solution(200);
+
+/**
+ * const dp = [1]로 초기화하고
+ * for (let i = 1; i + dp[i - 1] <= s; i++) {
+		dp[i] = dp[i - 1] + i;
+	}
+	로 돌려야 했을듯
+
+	근데 그냥 dp길이에서 -1 해버림
+ */
